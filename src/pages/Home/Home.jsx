8c158c6ead61aa72e9c0f68f9649f5a47b9380ea
Akshat Mittal <akshatmittal2506@gmail.com>
2022-06-17 18:57:49 +0530

    added multicolor border

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,18 +6,26 @@ import "./home.css";
 const Home = () => {
 	return (
 		<main className="home">
-			<div className="hero">
-				<div className="hero-image">
-					<img src={avatar} alt="Akshat Mittal" data-aos="zoom-in" />
-				</div>
-				<div className="hero-text">
-					<Typing>Hey there, I'm Akshat Mittal</Typing>
-					<span>
-						<img
-							src="https://raw.githubusercontent.com/MartinHeinz/MartinHeinz/master/wave.gif"
-							alt="Wave"
-						/>
-					</span>
+			<div className="card" data-aos="zoom-out">
+				<div className="card-box">
+					<div className="hero">
+						<div className="hero-image">
+							<img
+								src={avatar}
+								alt="Akshat Mittal"
+								data-aos="zoom-in"
+							/>
+						</div>
+						<div className="hero-text">
+							<Typing>Hey there, I'm Akshat Mittal</Typing>
+							<span>
+								<img
+									src="https://raw.githubusercontent.com/MartinHeinz/MartinHeinz/master/wave.gif"
+									alt="Wave"
+								/>
+							</span>
+						</div>
+					</div>
 				</div>
 			</div>
 		</main>
